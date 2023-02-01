f1f43c9e5479f771ed8e9aafdd4db16545419d0f
Roy <oisroy233@gmail.com>
2023-02-01 17:30:43 +0800

    修复语言判断错误的问题

--- languages/language.go
+++ languages/language.go
@@ -98,7 +98,7 @@ func Get(update *tgbotapi.Update) *LanguageStruct {
 	if update.Message != nil && lang[update.Message.From.LanguageCode] != nil {
 		return lang[update.Message.From.LanguageCode]
 	} else if update.CallbackQuery != nil && lang[update.CallbackQuery.Message.From.LanguageCode] != nil {
-		return lang[update.CallbackQuery.Message.From.LanguageCode]
+		return lang[update.CallbackQuery.From.LanguageCode]
 	}
 
 	//no matched language, return default language
